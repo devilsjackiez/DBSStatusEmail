de746faea560aaded00bdc3ef34aab589eaf0305
Jackrin Patanasutakit <j.patanasutakit@clbs.co.th>
2017-02-16 10:07:55 +0700

    Initial commit

---
+++ Template1Cal.php
@@ -0,0 +1,79 @@
+<?php
+
+include "ConnectDB.php";
+
+// Query status OPEN
+$sql = "SELECT `template_version`,count(`action`)FROM sugarcrm_hr.email_tracking WHERE template_version='DBS2016V1' && `action`='open'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+
+    while ($row = $result->fetch_assoc()) {
+
+        /*echo "OPEN = " . $row['count(`action`)'];*/
+
+        $_SESSION['templateVersion'] = $row['template_version'];
+        $_SESSION['ActionCount'] = $row['count(`action`)'];
+
+    }
+} else {
+    echo "0 results";
+}
+// Query status Unread
+$sql = "SELECT `template_version`,count(`action`)FROM sugarcrm_hr.email_tracking WHERE template_version='DBS2016V1' && `action`='unread'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+
+    while ($row = $result->fetch_assoc()) {
+        /*echo "Unread  = " . $row['count(`action`)'];*/
+
+        /*$_SESSION['templateVersion'] = $row['template_version'];*/
+        $_SESSION['ActionCountUnread'] = $row['count(`action`)'];
+
+    }
+} else {
+    echo "0 results";
+}
+// Query status Repiled
+$sql = "SELECT `template_version`,count(`action`)FROM sugarcrm_hr.email_tracking WHERE template_version='DBS2016V1' && `action`='replied'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+
+    while ($row = $result->fetch_assoc()) {
+        /*echo "Unread  = " . $row['count(`action`)'];*/
+
+        /*$_SESSION['templateVersion'] = $row['template_version'];*/
+        $_SESSION['ActionCountReplied'] = $row['count(`action`)'];
+
+    }
+} else {
+    echo "0 results";
+}//DBSQUERY
+// Query status Click
+$sql = "SELECT `template_version`,count(`action`)FROM sugarcrm_hr.email_tracking WHERE template_version='DBS2016V1' && `action`='click'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+
+    while ($row = $result->fetch_assoc()) {
+
+        /*echo "OPEN = " . $row['count(`action`)'];*/
+        $_SESSION['ActionCountClick'] = $row['count(`action`)'];
+
+    }
+} else {
+    echo "0 results";
+}
+// Calculate all of thing ------------------------------------------
+$Read = $_SESSION['ActionCountClick']+$_SESSION['ActionCount'];
+$Unread = $_SESSION['ActionCountUnread'];
+$Replied = $_SESSION['ActionCountReplied'];
+$Total = $_SESSION['ActionCountUnread'] + $_SESSION['ActionCount']+$_SESSION['ActionCountClick'] + $_SESSION['ActionCountReplied'];
---
+++ avg/header.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" id="csstyle">
+<head>
+    <title>Email Template Dashbord</title>
+    <link
+        href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic'
+        rel='stylesheet' type='text/css'/>
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+    <!--<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<div class="container-fluid" style="padding-top:10px;">
+    <br>
+    <img class="img-responsive" alt="Brand" src="../img/DBSlogo.png" width="210px" height="125px">
+    <br>
+</div>
+</body>
